a7ccef7c8da75d6e1526fb5806c1187bb51a91b5
pvquang89 <phamvanquang12042002@gmail.com>
2024-12-27 13:30:26 +0700

    [Feature] Configure Identity

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
+﻿using IdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configuration Identity Services
+//AddIdentity() : phương thức dùng để cấu hình và thiết lập identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    //cấu hình tuỳ chỉnh yêu cầu password
+    options.Password.RequireNonAlphanumeric = false;     // Không cần ký tự đặc biệt
+    options.Password.RequireUppercase = false;           // Không cần chữ hoa
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+//Configure Entity Framework Core
+var connectionString = builder.Configuration.GetConnectionString("SQLServerIdentityConnection") ?? throw new InvalidOperationException("Connection string 'SQLServerIdentityConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+
 
 var app = builder.Build();
 
@@ -18,6 +39,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//Configuring Authentication Middleware to the Request Pipeline
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
